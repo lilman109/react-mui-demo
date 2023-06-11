28dd563522cd1e2a58a6d842a4afe540f9c8444b
lilman109 <akira.yoshidasjsu@gmail.com>
2023-06-11 11:41:49 +0900

    typography component

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,43 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+      <Typography variant="subtitle1">Sub title 1</Typography>
+      <Typography variant="subtitle2">Sub title 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+        et culpa duis.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+        enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+        exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit
+        nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
+        minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure
+        elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor
+        Lorem duis laboris cupidatat officia voluptate. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod.
+        Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa
+        et culpa duis.
+      </Typography>
+    </div>
+  );
+};
