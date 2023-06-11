ffb0c581aa55f2ac548639eda24aa729337fff6c
lilman109 <akira.yoshidasjsu@gmail.com>
2023-06-11 17:38:53 +0900

    radio button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import MuiButton from './components/MuiButton';
+import MuiRadioButton from './components/MuiRadioButton';
 import MuiSelect from './components/MuiSelect';
 import MuiTextField from './components/MuiTextField';
 // import { MuiTypography } from './components/MuiTypography';
@@ -10,7 +11,8 @@ function App() {
       {/* <MuiTypography/> */}
       {/* <MuiButton/> */}
       {/* <MuiTextField/> */}
-      <MuiSelect/>
+      {/* <MuiSelect/> */}
+      <MuiRadioButton/>
     </div>
   );
 }
---
+++ src/components/MuiRadioButton.tsx
@@ -0,0 +1,48 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  FormHelperText,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const MuiRadioButton = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  return (
+    <Box>
+      <FormControl error>
+        <FormLabel id="job-experience-group-label">
+          Years of experience
+        </FormLabel>
+        <RadioGroup
+          name="job-experience-group"
+          aria-labelby="job-experience-group-label"
+          value={value}
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            control={<Radio size="small" color="secondary" />}
+            label="0-2"
+            value="0-2"
+          />
+          <FormControlLabel
+            control={<Radio size="medium" />}
+            label="3-5"
+            value="3-5"
+          />
+          <FormControlLabel control={<Radio />} label="6-10" value="6-10" />
+        </RadioGroup>
+        <FormHelperText>Invalid selection</FormHelperText>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default MuiRadioButton;
